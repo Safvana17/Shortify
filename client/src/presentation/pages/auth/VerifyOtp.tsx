02c983c2cbd3054a82c6e40ec1a893231454a738
Safvana V <safvanasafrab@gmail.com>
2026-06-26 20:01:48 +0530

    feat: add verify OTP flow with redux logic and UI

--- client/src/presentation/pages/auth/VerifyOtp.tsx
+++ client/src/presentation/pages/auth/VerifyOtp.tsx
@@ -1,16 +1,19 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect, useRef} from "react";
 import Logo from "../../components/Logo";
 import { ROUTES } from "../../../constants/routes";
 import { useLocation, useNavigate } from "react-router-dom";
-// import { useDispatch } from "react-redux";
-// import type { AppDispatch } from "../../../redux/store";
+import { useDispatch } from "react-redux";
+import type { AppDispatch } from "../../../redux/store";
+import toast from "react-hot-toast";
+import { verifyOtp } from "../../../redux/slices/authSlice";
 
 
 const VerifyOtp: React.FC =()=>{
 
     const [time, setTime] = useState(120)
-    // const [otp, setOtp] = useState<string[]>(Array(6).fill(''))
-    // const dispatch = useDispatch<AppDispatch>()
+    const [otp, setOtp] = useState<string[]>(Array(6).fill(''))
+    const dispatch = useDispatch<AppDispatch>()
+    const inputRef = useRef<(HTMLInputElement | null)[]>([])
     const location = useLocation()
     const navigate = useNavigate()
     const email = location.state?.email
@@ -37,6 +40,49 @@ const VerifyOtp: React.FC =()=>{
         return `0${minutes} :${reminingSeconds < 10 ? ' 0' : ' '}${reminingSeconds}`
     }
 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+        const value = e.target.value
+        if(isNaN(Number(value))) return false
+
+        const newOtp = [...otp]
+        newOtp[index] = value
+        setOtp(newOtp)
+        if(index < 5 && value){
+            inputRef.current[index + 1]?.focus()
+        }
+    }
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, index: number) => {
+        if(e.key === 'Backspace'){
+            if(otp[index]){
+                const newOtp = [...otp]
+                newOtp[index] = ''
+                setOtp(newOtp)
+            }else if(index > 0){
+                inputRef.current[index - 1]?.focus()
+            }
+        }
+    }
+
+    const handleSubmit = async() => {
+        const otpValue = otp.join('')
+        try {
+            await dispatch(verifyOtp({email, otp: otpValue})).unwrap()
+            toast.success('Your OTP has been verified successfully, Please login to continue')
+            navigate(ROUTES.PUBLIC.LOGIN)
+        } catch (error) {
+            toast.error(typeof error === 'string' ? error : 'Failed to verify otp')
+        }
+    }
+
+    const handlePaste = (e: React.ClipboardEvent<HTMLInputElement>) => {
+        const pasteData = e.clipboardData.getData('text').slice(0, 6)
+        if(!/^\d+$/.test(pasteData)) return 
+        const newOtp = pasteData.split('')
+        setOtp([...newOtp, ...Array(6 - newOtp.length).fill('')])
+        inputRef.current[Math.min(pasteData.length, 5)]?.focus()
+    }
+
 return (
     <div className="min-h-screen bg-gradient-to-br from-indigo-50 via-white to-purple-50 flex flex-col items-center">
         <div className="mt-20">
@@ -49,12 +95,20 @@ return (
             <p className="text-gray-500 mt-3">
                 Enter the 6 digit code sent to your email
             </p>
-            <div className="flex justify-center gap-4 mt-8">
-            { [1,2,3,4,5,6].map(i=>(
-                <input
-                    key={i}
-                    maxLength={1}
-                    className="w-14 h-14 border rounded-lg text-center text-xl outline-none"
+            <div className='flex justify-between gap-3'>
+            { otp.map((data, index) => (
+                <input 
+                key={index}
+                ref={(el) => {inputRef.current[index] = el}}
+                type='text'
+                data-index={index}
+                maxLength={1}
+                value={data}
+                onChange={(e) => handleChange(e, index)}
+                onFocus={(e) => e.target.select()}
+                onKeyDown={(e) => handleKeyDown(e, index)}
+                onPaste={handlePaste}
+                className='w-12 h-14 bg-white border mt-5 border-gray-300 rounded-md text-center text-2xl font-bold text-black focus:outline-none transition focus:border-[#5C3C02]'
                 />
             ))}
             </div>
@@ -66,7 +120,10 @@ return (
                 className={`cursor-pointer mt-5 w-full py-3 rounded-lg text-white ${time===0 ? "bg-gradient-to-r from-indigo-500 to-purple-600" : "bg-gray-400"}`}>
                 Resend OTP
             </button>
-            <button className="cursor-pointer mt-4 w-full py-3 rounded-lg bg-gradient-to-r from-indigo-500 to-purple-600 text-white">
+            <button 
+                onClick={handleSubmit}
+                className="cursor-pointer mt-4 w-full py-3 rounded-lg bg-gradient-to-r from-indigo-500 to-purple-600 text-white"
+            >
                 Verify
             </button>
         </div>
--- client/src/redux/slices/authSlice.ts
+++ client/src/redux/slices/authSlice.ts
@@ -39,6 +39,25 @@ RegisterUserPayload,
     }
 })
 
+export const verifyOtp = createAsyncThunk<
+void,
+{email: string, otp: string},
+{rejectValue: string}
+>('user/verify-otp', async({email, otp}, {rejectWithValue}) => {
+    try {
+
+        const response = await api.post(API_ROUTES.AUTH.VERIFY_OTP, {email, otp})
+        if(!response.data.success){
+            return rejectWithValue("Invalid response")
+        }
+        return response.data.data
+
+    } catch (error) {
+        const err = error as AxiosError<{message: string}>
+        return rejectWithValue(typeof err === 'string' ? err : 'Failed to verify otp')
+    }
+})
+
 
 const authSlice = createSlice({
     name: 'AuthSlice',
@@ -55,6 +74,16 @@ const authSlice = createSlice({
           .addCase(registerUser.rejected, (state, action) => {
             state.loading = false
             state.error = action.payload || 'Failed to register user'
+          })
+          .addCase(verifyOtp.pending, (state) => {
+            state.loading = true
+          })
+          .addCase(verifyOtp.fulfilled, (state) => {
+            state.loading = false
+          })
+          .addCase(verifyOtp.rejected, (state, action) => {
+            state.loading = false
+            state.error = action.payload || 'Failed to verify otp'
           })
     }
 })
